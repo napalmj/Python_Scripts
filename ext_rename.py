9224addac6f51b07b4d99430fda9c8f7aa9e4834
Nathaniel Palmer <61099489+napomj@users.noreply.github.com>
2021-02-10 20:11:15 -0800

    Add files via upload

--- ext_rename.py
+++ ext_rename.py
@@ -1,23 +1,36 @@
 '''
 ----------------------------------------------------------------------------------------------
  Script Usage: Renames all extensions specified by the user to the extension the user chooses
+ How to use: script run via command line. first arg is current ext, second arg is final ext.
  Version: python 3.8.7
 ----------------------------------------------------------------------------------------------
 '''
 
 #!/usr/bin/env python3
 import os
+import sys
+
+#checks for correct user input
+def check_dot(ext):
+	temp = list(ext)
+	if temp[0] != '.':
+		print('Extensions must begin with a Dot --> .\n')
+		print('Nothing implimented.\nExiting...')
+		exit()
+
+ext_current = sys.argv[1]
+ext_final = sys.argv[2]
+
+#call function
+check_dot(ext_current)
+check_dot(ext_final)
 
 #gets current directory
 current_dir = os.getcwd()
 
-#takes user input 
-userExtOriginal = input("Original extension name to be reassigned: ")
-userExtFinal = input("Final extension name to be assigned: ")
-
 for file in os.listdir(current_dir): #listdir
 	fileName, fileExt = os.path.splitext(file) #splitting file text
-	if fileExt == userExtOriginal: #seperate all file extensions with cxx
-		fileExt = userExtFinal #overwrite to cpp
+	if fileExt == ext_current: #seperate all file extensions with cxx
+		fileExt = ext_final #overwrite to cpp
 	newExt = '{}{}'.format(fileName,fileExt) #resetting format
 	os.rename(file, newExt) #resetting to directory with rename
