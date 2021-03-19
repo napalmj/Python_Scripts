a3e1911befd7fc6528f1c7016000871369a512d0
Nathaniel Palmer <61099489+napomj@users.noreply.github.com>
2021-03-18 18:00:35 -0700

    Rename Image
    Script works. A few things to be ironed out still but functions.

--- image_rename.py
+++ image_rename.py
@@ -11,7 +11,10 @@ Lib Details:
 	Pillow library will need to be installed for script to work
 	Reference -> pillow.readthedocs.io
 Progress:
-	Still working on script
+	It works: 3/18/2021
+	A few bugs still to work out.
+	--Logic with comparing dates.
+	--Also, I need to add a function to detect already named files which the user wants to rename the files too. Otherwise throws error
 ---------------------------------------------------------------------------------------------------------------------
 '''
 
@@ -19,10 +22,14 @@ Progress:
 import os
 import sys
 from PIL import Image
-im = Image.open("Married.jpg").getexif()[36867]
-
+#im = Image.open("Married.jpg").getexif()[36867]
+  
 #establishing current directory
 current_directory = os.getcwd()
+
+start_month = ''
+end_month = ''
+file_name = ''
 
 #declaring our argument values
 if len(sys.argv) == 4:
@@ -30,12 +37,15 @@ if len(sys.argv) == 4:
 	end_month = sys.argv[2]
 	file_name = sys.argv[3]
 	print("Initial Date: ", start_month, "\nFinal Date:", end_month, "\nName for File:", file_name)
+else:
+	print('Incorrect Input')
+
 
 #---------------------
 #function definitions
 #---------------------
 
-#checks if the extension matches a image file extension
+#checks if the extension matches a image file extension  
 def checkExt(ext):
 	ext = ext.lower() #insures all extensions are lowercase
 	extensionList = ['.png', '.jpg', '.raw']
@@ -55,7 +65,7 @@ def str2int(dataList):
 	return dataList
 
 #splits up date string
-def parseDateStr(dateStr):
+def imageDateStr(dateStr):
 	strHolder = ''
 	data = dateStr.split(' ')
 	date = strHolder.join(data[0])
@@ -66,15 +76,28 @@ def parseDateStr(dateStr):
 
 	return dateTimeData
 
-#input dates
+#input date parsing for range
+def inputDateStr(dateStr1, dateStr2):
+	start_date = dateStr1.split(':')
+	end_date = dateStr2.split(':')
+	dateRange = [start_date, end_date]
+	return dateRange
+
+#compares the dates to see if image date is in user range
+def compareDates(rangeDate, imageDate):
+	isInRange = False
+	for n in range(0, 3, 1):
+		if int(rangeDate[0][n]) <= int(imageDate[0][n]) and int(rangeDate[1][n]) >= int(imageDate[0][n]):
+			isInRange = True
+			break
+	return isInRange
 
 
 #---------------
 #code execution
 #---------------
-
-
-
+rangeDate = inputDateStr(start_month, end_month)
+count = 0
 #goes through director
 for file in os.listdir(current_directory):
 	tempFile = file
@@ -82,8 +105,13 @@ for file in os.listdir(current_directory):
 
 	if checkExt(fileExt):
 		im = Image.open(file).getexif()[36867]
-		print(file, im)
-		parseDateStr(im)
+
+		imageDate = imageDateStr(im)
+
+		if compareDates(rangeDate, imageDate):
+			newName = '{}_{}{}'.format(file_name, count, fileExt)
+			os.rename(file, newName)
+			count += 1
 
 
 
